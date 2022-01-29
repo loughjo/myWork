a7c81ed4621ebeeb30a464351b49ee68ae47e2a6
unknown <G00388113@gmit.ie>
2022-01-29 21:27:45 +0000

    week 02 nameAndAge program

---
+++ week02/nameAndAge.py
@@ -0,0 +1,9 @@
+# Author : John Loughnane
+# A program that takes in a name and age and prints it 
+
+# A user enters tehre anme and age 
+name = input('Please Enter your name : ')
+age = input('Please Enter your age : ')
+
+# Print out the name an age
+print('\nHello ' + name + ', your age is ' + str(age))
